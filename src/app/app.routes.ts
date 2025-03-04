78a2361c51ea5de6794f87a9ed819036c9691f9f
Adrian San Segundo <adrisanse.ss@gmail.com>
2025-03-04 16:16:08 +0100

    chore: refactor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LandingComponent } from './landing/landing.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { LandingComponent } from './components/pages/landing/landing.component';
 
 export const routes: Routes = [
   {
