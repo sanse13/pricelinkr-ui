02ea0477e80207c782108dfc6fa1ac47f3ecdd67
Adrian San Segundo <adrisanse.ss@gmail.com>
2025-03-05 14:02:49 +0100

    feat: add home page

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 
+interface Category {
+  name: string;
+}
+
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  categories: Category[] = [];
+}
