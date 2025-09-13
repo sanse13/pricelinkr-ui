c078cb1e3d0b87c1ec4d374440afedf117e22803
Adrian San Segundo <adrisanse.ss@gmail.com>
2025-09-13 17:42:48 +0200

    feat: add login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
     path: '',
     loadComponent: () =>
       import('./components/pages/landing/landing.component').then(
